e72893bf9118fd9d1d9009d6ffe32775991726e3
wdd <296151852@qq.com>
2021-05-23 22:04:02 +0800

    1

--- src/page/achievement/yelingxi/components/suwan/001.vue
+++ src/page/achievement/yelingxi/components/suwan/001.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="ye-model">
     <div class="ye-model-cen">
-      人物
-      师承
-      武功绝技
-      相关好友
-      成名事迹
+      <h2>苏晚</h2>
+      <p>师承：不详</p>
+      <p>武功绝技：回天绝响、百毒魔功</p>
+      <p>相关好友：夜灵犀</p>
+      <p>成名事迹：月与楼中的花魁，身份神秘。</p>
     </div>
   </div>
 </template>
@@ -34,6 +34,16 @@
     color: #fff;
     .ye-model-cen {
       padding: .8rem;
+      h2 {
+        margin: 0;
+        padding: 0 0 0.6rem;
+        font-size: .9rem;
+      }
+      p {
+        margin: 0;
+        padding: 0 0 0.6rem;
+        font-size: .8rem;
+      }
     }
   }
 </style>
--- src/page/achievement/yelingxi/components/yelingxi/001.vue
+++ src/page/achievement/yelingxi/components/yelingxi/001.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="ye-model">
     <div class="ye-model-cen">
-      人物
-      师承
-      武功绝技
-      相关好友
-      成名事迹
+      <h2>夜灵犀</h2>
+      <p>师承：宁云天</p>
+      <p>武功绝技：燕云七绝、乾坤生死诀</p>
+      <p>相关好友：苏晚、百里梦、妙乐</p>
+      <p>成名事迹：十八岁，入少林与少林方丈恒业大师比武，一战成名！</p>
     </div>
   </div>
 </template>
@@ -34,6 +34,16 @@
     color: #fff;
     .ye-model-cen {
       padding: .8rem;
+      h2 {
+        margin: 0;
+        padding: 0 0 0.6rem;
+        font-size: .9rem;
+      }
+      p {
+        margin: 0;
+        padding: 0 0 0.6rem;
+        font-size: .8rem;
+      }
     }
   }
 </style>
